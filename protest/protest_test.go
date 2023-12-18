0d008da0d1aa6a5e13a2035886851ff6316c5ce2
toejough <toejough@gmail.com>
2023-12-17 21:23:14 -0500

    refactor: move mockedtestingT down below last test to use it.

--- protest/protest_test.go
+++ protest/protest_test.go
@@ -69,18 +69,6 @@ func TestStartFailsCleanlyWithTooFewArgs(t *testing.T) {
 	}
 }
 
-// MockedTestingT.
-func newMockedTestingT() *mockedTestingT { return &mockedTestingT{failure: ""} }
-
-type mockedTestingT struct{ failure string }
-
-func (mt *mockedTestingT) Fatalf(message string, args ...any) {
-	mt.failure = fmt.Sprintf(message, args...)
-}
-func (mt *mockedTestingT) Helper()         {}
-func (mt *mockedTestingT) Failed() bool    { return mt.failure != "" }
-func (mt *mockedTestingT) Failure() string { return mt.failure }
-
 func TestStartFailsCleanlyWithTooManyArgs(t *testing.T) {
 	t.Parallel()
 
@@ -109,6 +97,18 @@ func TestStartFailsCleanlyWithTooManyArgs(t *testing.T) {
 		)
 	}
 }
+
+// MockedTestingT.
+func newMockedTestingT() *mockedTestingT { return &mockedTestingT{failure: ""} }
+
+type mockedTestingT struct{ failure string }
+
+func (mt *mockedTestingT) Fatalf(message string, args ...any) {
+	mt.failure = fmt.Sprintf(message, args...)
+}
+func (mt *mockedTestingT) Helper()         {}
+func (mt *mockedTestingT) Failed() bool    { return mt.failure != "" }
+func (mt *mockedTestingT) Failure() string { return mt.failure }
 
 // func TestRepeatedCalls(t *testing.T) {
 // 	t.Parallel()
