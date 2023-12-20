dee09d6946f2c0fb2e248fbc699680c684d687b4
toejough <toejough@gmail.com>
2023-12-20 00:32:21 -0500

    test: test assertNextCallIs fails with wrong func

--- tests/testerAssertNextCallIs_test.go
+++ tests/testerAssertNextCallIs_test.go
@@ -1,6 +1,7 @@
 package protest_test
 
 import (
+	"strings"
 	"testing"
 	"time"
 
@@ -46,43 +47,51 @@ func newTestDepsMock(t *protest.RelayTester) *testDepsMock {
 
 func (tdm *testDepsMock) Func() { tdm.tester.PutCall(tdm.Func) }
 
-// func TestAssertNextCallIsWrongFuncFails(t *testing.T) {
-// 	t.Parallel()
-// 	panic("not implemented")
-//
-// 	// Given test needs
-// 	mockedt := newMockedTestingT()
-// 	tester := protest.NewTester(mockedt)
-// 	// Given inputs
-// 	returns := func() int {
-// 		return 5
-// 	}
-//
-// 	// When the func is run
-// 	tester.Start(returns)
-//
-// 	// And we wait for it to finish
-// 	tester.AssertDoneWithin(time.Second)
-//
-// 	// And we expect it to return the right value
-// 	tester.AssertReturned(6)
-//
-// 	// Then the test is marked as failed
-// 	if !mockedt.Failed() {
-// 		t.Fatal(
-// 			"The test should've failed with wrong values. Instead the test passed!",
-// 		)
-// 	}
-// 	// Then the error calls out wrong value
-// 	if !strings.Contains(mockedt.Failure(), "wrong value") {
-// 		t.Fatalf(
-// 			"The test should've failed with wrong value returned. Instead the failure was: %s",
-// 			mockedt.Failure(),
-// 		)
-// 	}
-// }
+func TestAssertNextCallIsWrongFuncFails(t *testing.T) {
+	t.Parallel()
 
-// TODO: test that AssertNextCallIs passes if the call & args match
+	// Given test needs
+	mockedt := newMockedTestingT()
+	tester := protest.NewTester(mockedt)
+	// Given inputs
+	returns := func(deps testDeps) {
+		deps.Func()
+	}
+	tdm := newTestDepsMockWrongFunc(tester)
+
+	// When the func is run
+	tester.Start(returns, tdm)
+	// and nice cleanup is scheduled
+	defer tester.AssertDoneWithin(time.Second)
+
+	// Then the next call is to the func
+	tester.AssertNextCallIs(tdm.Func)
+
+	// Then the test is marked as failed
+	if !mockedt.Failed() {
+		t.Fatal(
+			"The test should've failed with wrong func called. Instead the test passed!",
+		)
+	}
+	// Then the error calls out wrong value
+	if !strings.Contains(mockedt.Failure(), "wrong func") {
+		t.Fatalf(
+			"The test should've failed with wrong func called. Instead the failure was: %s",
+			mockedt.Failure(),
+		)
+	}
+}
+
+type testDepsMockWrongFunc struct{ tester *protest.RelayTester }
+
+func newTestDepsMockWrongFunc(t *protest.RelayTester) *testDepsMockWrongFunc {
+	return &testDepsMockWrongFunc{tester: t}
+}
+
+func (tdm *testDepsMockWrongFunc) Func() { tdm.tester.PutCall(wrongFunc) }
+
+func wrongFunc() {}
+
 // TODO: test that AssertNextCallIs fails if the call is wrong
 // TODO: test that AssertNextCallIs fails if the args are the wrong type
 // TODO: test that AssertNextCallIs fails if the args are the wrong number
