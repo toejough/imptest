15150a9eb9078505714c39ffcb1a7a442b91ec13
toejough <toejough@gmail.com>
2023-12-23 22:26:19 -0500

    test: add test for fillreturn failing on wrong number

--- tests/testerFillReturns_test.go
+++ tests/testerFillReturns_test.go
@@ -40,6 +40,39 @@ func (tdm *testDepsMock) FillWrongType() int {
 	return goodR
 }
 
+func TestFillReturnWrongNumberFails(t *testing.T) {
+	t.Parallel()
+
+	// Given test needs
+	mockedt := newMockedTestingT()
+	tester := protest.NewTester(mockedt)
+	// Given inputs
+	returns := func(deps testDepsFillWrongNumber) int {
+		// Then test fails with wrong return type
+		defer expectPanicWith(t, "wrong number of returns")
+		return deps.FillWrongNumber()
+	}
+	tdm := newTestDepsMock(tester)
+
+	// When the func is run
+	tester.Start(returns, tdm)
+	tester.AssertNextCallIs(tdm.FillWrongNumber).InjectReturns(5)
+	tester.AssertDoneWithin(time.Second)
+}
+
+type testDepsFillWrongNumber interface{ FillWrongNumber() int }
+
+func (tdm *testDepsMock) FillWrongNumber() int {
+	var (
+		goodR int
+		badR  string
+	)
+
+	tdm.tester.PutCall(tdm.FillWrongType).FillReturns(&goodR, &badR)
+
+	return goodR
+}
+
 // TODO: test that FillReturns fails if the args are the wrong number for the call
 // TODO: test parallel calls
 // TODO: refactor protest out into separate files
