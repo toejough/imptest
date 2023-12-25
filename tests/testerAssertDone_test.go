bf93fb8060d4fd029c6e6c7ecdf88e3e699b515c
toejough <toejough@gmail.com>
2023-12-25 18:33:59 -0500

    test: add test for done called with queued call

--- tests/testerAssertDone_test.go
+++ tests/testerAssertDone_test.go
@@ -68,3 +68,41 @@ func TestAssertDonePassesIfDone(t *testing.T) {
 		)
 	}
 }
+
+func TestAssertDoneWithQueuedCallFails(t *testing.T) {
+	t.Parallel()
+
+	// Given test needs
+	mockedt := newMockedTestingT()
+	tester := protest.NewTester(mockedt)
+	// Given inputs
+	wait := func(deps testDepsQueuedCall) {
+		deps.QueuedCall()
+	}
+	tdm := newTestDepsMock(tester)
+
+	// When the func is run
+	tester.Start(wait, tdm)
+
+	// And we wait for it to finish
+	tester.AssertDoneWithin(time.Second)
+
+	// Then the test is marked as failed
+	if !mockedt.Failed() {
+		t.Fatal("The test should've failed due to a queued call, but it didn't")
+	}
+
+	// Then the test has the right error message
+	if !strings.Contains(mockedt.Failure(), "had a call queued") {
+		t.Fatalf(
+			"The test should've failed due to a queued call, but it didn't. Instead the failure was: %s",
+			mockedt.Failure(),
+		)
+	}
+}
+
+type testDepsQueuedCall interface{ QueuedCall() }
+
+func (tdm *testDepsMock) QueuedCall() {
+	tdm.tester.PutCall(tdm.QueuedCall)
+}
