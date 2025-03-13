433323386ccde7a48b8d03f6d49a5758b1fa0683
toejough <toejough@gmail.com>
2025-03-13 13:27:05 -0400

    green: increased mutation score to 91%

--- imptest/consts_test.go
+++ imptest/consts_test.go
@@ -1,7 +1,10 @@
 package imptest //nolint:testpackage
 // This package is intentionally a white-box test
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 // TestConsts tests consts to be exactly what I set them at. It's a little silly, but I don't see any other way to make
 // the mutation tester chill out about these values. They're not important as exact numbers, so without this test, the
@@ -15,5 +18,9 @@ func TestConsts(t *testing.T) {
 
 	if constDefaultActivityBufferSize != 100 {
 		t.Fatal("the default activity buffer size needs to be 100, but it's not.")
+	}
+
+	if defaultResolutionMaxDuration != 100*time.Millisecond {
+		t.Fatal("the default resolution max duration needs to be 100 milliseconds, but it's not.")
 	}
 }
