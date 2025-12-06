b61b7a62e98b3e8d1bf04b2b58310650dc62766c
toejough <toejough@gmail.com>
2025-12-05 21:14:15 -0500

    [me] add mutation tests

--- magefiles/magefile.go
+++ magefiles/magefile.go
@@ -243,7 +243,7 @@ func Test(c context.Context) error {
 		// "-shuffle=1725149006359140000",
 		"-race",
 		"-coverprofile=coverage.out",
-		// "-coverpkg=./imptest",
+		"-coverpkg=./POC-imptest-API/UAT/run,./POC-imptest-API/imptest",
 		"./...",
 		// -test.shuffle 1725149006359140000
 	)
@@ -261,7 +261,7 @@ func Mutate(c context.Context) error {
 				"test",
 				// "-v",
 				"-tags=mutation",
-				"./tests",
+				"./...",
 				"-run=TestMutation",
 				// "-ooze.v",
 			)
---
+++ mutation_test.go
@@ -0,0 +1,21 @@
+//go:build mutation
+
+package main_test
+
+import (
+	"testing"
+
+	"github.com/gtramontina/ooze"
+)
+
+func TestMutation(t *testing.T) {
+	ooze.Release(
+		t,
+		ooze.WithTestCommand("mage testForFail"),
+		ooze.Parallel(),
+		ooze.IgnoreSourceFiles("^magefiles.*|.*_string.go"),
+		// ooze.WithMinimumThreshold(0.95),
+		ooze.WithRepositoryRoot(".."),
+		ooze.ForceColors(),
+	)
+}
